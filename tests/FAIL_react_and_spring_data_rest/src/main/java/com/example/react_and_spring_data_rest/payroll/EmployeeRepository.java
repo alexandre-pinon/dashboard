9e0d53f15dc1ae56ca7e1b5da1a577f58b0dfcf8
alexandre-pinon <alexandre.pinon@epitech.eu>
2020-12-09 14:35:14 +0100

    rm FAIL_react_and_spring_data_rest

--- tests/FAIL_react_and_spring_data_rest/src/main/java/com/example/react_and_spring_data_rest/payroll/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.react_and_spring_data_rest.payroll;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface EmployeeRepository extends CrudRepository<Employee, Long> {
-    
-}

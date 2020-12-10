3b05c0a773ac5bd79aa09f877d519736b1b82e7b
alexandre-pinon <alexandre.pinon@epitech.eu>
2020-12-10 13:31:46 +0100

    added plugin for babel

--- tests/weather_react/webpack.config.js
+++ tests/weather_react/webpack.config.js
@@ -6,7 +6,8 @@ module.exports = {
             exclude: /node_modules/,
             loader: "babel-loader",
             options: {
-                presets: ['@babel/preset-env', '@babel/preset-react']
+                presets: ['@babel/preset-env', '@babel/preset-react'],
+                plugins: ['@babel/plugin-transform-runtime']
             }
         }, {
             test: /\.css$/,

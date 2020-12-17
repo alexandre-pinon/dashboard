f1009f0ba1fb1b230fa6ae413478d6a672e0166d
bryanseychelles <bryanseychellesyk@gmail.com>
2020-12-17 18:23:22 +0100

    drag and drop

--- FrontEnd/src/Component/Home/index.js
+++ FrontEnd/src/Component/Home/index.js
@@ -5,21 +5,20 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import ListGroup from 'react-bootstrap/ListGroup'
 import Button from 'react-bootstrap/Button'
 
-
 var edit = 'edit';
 var closer = 'closer';
 var cards = 'Card';
 
 const card = [
-    {id: 1,name : "Youtube" , img : "http://via.placeholder.com/640x360" },
-    {id: 2,name : "Twitch" , img : "http://via.placeholder.com/640x360" },
-    {id: 3,name : "Twitter" , img : "http://via.placeholder.com/640x360" },
-    {id: 4,name : "Google" , img : "http://via.placeholder.com/640x360" },
-    {id: 5,name : "GitHub" , img : "http://via.placeholder.com/640x360" },
-    {id: 6,name : "Facebook" , img : "http://via.placeholder.com/640x360" },
-    {id: 7,name : "Deezer" , img : "http://via.placeholder.com/640x360" },
-    {id: 8,name : "AppleMusic" , img : "http://via.placeholder.com/640x360" },
-    {id: 9,name : "Linkedin" , img : "http://via.placeholder.com/640x360" },
+    {id: 1, uid: 'Youtube1' , name : "Youtube" , img : "http://via.placeholder.com/640x360" },
+    {id: 2, uid: 'Twitch1' , name : "Twitch" , img : "http://via.placeholder.com/640x360" },
+    {id: 3, uid: 'Twitter1' , name : "Twitter" , img : "http://via.placeholder.com/640x360" },
+    {id: 4, uid: 'Google1' , name : "Google" , img : "http://via.placeholder.com/640x360" },
+    {id: 5, uid: 'GitHub1' , name : "GitHub" , img : "http://via.placeholder.com/640x360" },
+    {id: 6, uid: 'Facebook1' , name : "Facebook" , img : "http://via.placeholder.com/640x360" },
+    {id: 7, uid: 'Deezer1' , name : "Deezer" , img : "http://via.placeholder.com/640x360" },
+    {id: 8, uid: 'AppleMusic1' , name : "AppleMusic" , img : "http://via.placeholder.com/640x360" },
+    {id: 9, uid: 'Linkedin1' , name : "Linkedin" , img : "http://via.placeholder.com/640x360" },
 ]
 
 
@@ -39,50 +38,6 @@ function CloseOption(event) {
 
     elmt.style.display = 'none'
     edit.style.display = 'none'
-}
-
-function OpenEdit(event) {
-    var id = event.currentTarget.name
-    var div = edit+id
-    var elmt = document.getElementsByClassName(div)
-    elmt[0].style.display = 'block'
-}
-
-function CloseEdit(event) {
-    var id = event.currentTarget.id
-    var elmt = document.getElementById(id)
-    var panel = elmt.parentNode;
-    panel.parentNode.style.display = 'none'
-    var lastC = id.charAt(id.length-1)
-    var key = event.currentTarget.name
-
-    switch (key) {
-        case 'small':
-            var item = document.getElementById(cards+lastC)
-            console.log('enter in small', item)
-            item.style.width = '32%'
-            item.style.height = '30%'
-            break;
-        case 'medium':
-            var item = document.getElementById(cards+lastC)
-            var itemImg = document.getElementById("img"+lastC)
-            console.log('enter in medium', item)
-            item.style.height = '80vh'
-            itemImg.style.height = '80%'
-
-            break;
-        case 'tall':
-            var item = document.getElementById(cards+lastC)
-            var itemImg = document.getElementById("img"+lastC)
-            console.log('enter in tall' , item , itemImg)
-            item.style.width = '65%'
-            item.style.height = '80vh'
-            itemImg.style.height = '80%'
-            break;
-    
-        default:
-            break;
-    }
 }
 
 function DeleteCard(event) {
@@ -147,25 +102,68 @@ const ParamsWidget = () => {
     )
 }
 
+var col = {
+    [1]: {
+      items: card
+    },
+    [2]: {
+      items: []
+    },
+    [3]: {
+      items: []
+    },
+  };
+
+
+  const onDragEnd = (result, columns, setColumns) => {
+    if (!result.destination) return;
+    const { source, destination } = result;
+  
+    if (source.droppableId !== destination.droppableId) {
+      const sourceColumn = columns[source.droppableId];
+      const destColumn = columns[destination.droppableId];
+      const sourceItems = [...sourceColumn.items];
+      const destItems = [...destColumn.items];
+      const [removed] = sourceItems.splice(source.index, 1);
+      destItems.splice(destination.index, 0, removed);
+      setColumns({
+        ...columns,
+        [source.droppableId]: {
+          ...sourceColumn,
+          items: sourceItems
+        },
+        [destination.droppableId]: {
+          ...destColumn,
+          items: destItems
+        }
+      });
+    } else {
+      const column = columns[source.droppableId];
+      const copiedItems = [...column.items];
+      const [removed] = copiedItems.splice(source.index, 1);
+      copiedItems.splice(destination.index, 0, removed);
+      setColumns({
+        ...columns,
+        [source.droppableId]: {
+          ...column,
+          items: copiedItems
+        }
+      });
+    }
+  };
+
+  console.log(document.getElementsByClassName('card'))
 
 export const DashBoard = () => {
-    const [characters, updateCharacters] = useState(card)
 
-    function handleOnDragEnd(result) {
-        if (!result.destination) return;
-    
-        const items = Array.from(characters);
-        const [reorderedItem] = items.splice(result.source.index, 1);
-        items.splice(result.destination.index, 0, reorderedItem);
-    
-        updateCharacters(items);
-      }
+    const [columns, setColumns] = useState(col);
+
     return(
         <section className="container">
             <div id="addwidget" className="row">
                 <ParamsWidget/>
             </div>
-            <div className="row">
+            <div className="row" style={{ display: "flex", justifyContent: "center", height: "100%" }}>
                 <div className="container md-4">
                     <Button id='buttonOpen' onClick={OpenAddWidget} style={{width : '100%'}} variant="light" type="submit">
                         +
@@ -173,69 +171,76 @@ export const DashBoard = () => {
                     <Button id='buttonClose' onClick={CloseAddWidget} style={{width : '100%'}} variant="light" type="submit">
                         close
                     </Button>
-                    <DragDropContext onDragEnd={handleOnDragEnd}> 
-                        <Droppable droppableId= "containCard">
-                        { (provided) => (
-                            <div id="containCard" {...provided.droppableProps} ref={provided.innerRef}>
-                                {card.map( (item , index) => (
-                                    <Draggable key={"Card"+item.id} draggableId={"Card"+item.id} index={index} >
-                                    {(provided)=>(
-                                        <Card id={"Card"+item.id} {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
-                                            <Card.Img id={'img' + item.id} className='img' variant="top" src={item.img} />
-                                            <div className={"options"} id={item.id}>
-                                                <div className={edit + item.id} id="edit">
-                                                    <ListGroup>
-                                                        <ListGroup.Item id={closer + item.id} name={'small'} onClick={CloseEdit} action variant="info">
-                                                            small
-                                                        </ListGroup.Item>
-                                                        <ListGroup.Item id={closer + item.id} name={'medium'} onClick={CloseEdit} action variant="info">
-                                                            medium
-                                                        </ListGroup.Item>
-                                                        <ListGroup.Item id={closer + item.id} name={'tall'} onClick={CloseEdit} action variant="info">
-                                                            tall
-                                                        </ListGroup.Item>
-                                                    </ListGroup>
-                                                </div>
-                                                <ListGroup>
-                                                        <ListGroup.Item action variant="secondary">
-                                                            Application
-                                                        </ListGroup.Item>
-                                                        <ListGroup.Item onClick={OpenEdit} name={item.id} action variant="secondary">
-                                                            Edit
-                                                        </ListGroup.Item>
-                                                        <ListGroup.Item onClick={DeleteCard} name={item.id} action variant="secondary">
-                                                            Delete
-                                                        </ListGroup.Item>
-                                                </ListGroup>
-                                            </div>
-                                            <Card.Body>
-                                            <Card.Text>
-                                            <div className="row">
-                                                <div className='col-8'>
-                                                    {item.name}
-                                                </div>
-                                                <div className="col-2">
-                                                    <button name={item.id} onClick={CloseOption} className="btn btn-light">
-                                                        <FontAwesomeIcon icon="arrow-up"/>
-                                                    </button>
-                                                </div>
-                                            <div className="col-2">
-                                                    <button name={item.id} onClick={OpenOption} style={{display : 'block'}} className="btn btn-light">
-                                                        <FontAwesomeIcon icon="arrow-down"/>
-                                                    </button>
-                                                </div> 
-                                            </div>
-                                            </Card.Text>
-                                            </Card.Body>
-                                        </Card>
-                                    )}
-                                    </Draggable>
-                                ))}
-                            </div>
-                        )}
-                        </Droppable>
-                    </DragDropContext>
                 </div>
+                <DragDropContext onDragEnd={result => onDragEnd(result, columns, setColumns)}>
+                    {Object.entries(columns).map(([columnId, column], index) => (
+                        <Droppable droppableId={columnId} key={columnId}>
+                            {(provided, snapshot) => {
+                                return (
+                                    <div
+                                        className="col-sm" key={columnId}
+                                        {...provided.droppableProps}
+                                        ref={provided.innerRef}
+                                        style={{margin : 8}}
+                                    >
+                                        {card.map((item, index) => {
+                                            return (
+                                                <Draggable
+                                                    key={item.uid}
+                                                    draggableId={item.uid}
+                                                    index={index}
+                                                    column={columnId}
+                                                >
+                                                    {(provided, snapshot) => {
+                                                        return (
+                                                            <div
+                                                                ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}
+                                                            >
+                                                                <Card id={"Card"+item.id} >
+                                                                    <Card.Img id={'img' + item.id} className='img' variant="top" src={item.img} />
+                                                                    <div className={"options"} id={item.id}>
+                                                                        <ListGroup>
+                                                                                <ListGroup.Item action variant="secondary">
+                                                                                    Application
+                                                                                </ListGroup.Item>
+                                                                                <ListGroup.Item onClick={DeleteCard} name={item.id} action variant="secondary">
+                                                                                    Delete
+                                                                                </ListGroup.Item>
+                                                                        </ListGroup>
+                                                                    </div>
+                                                                    <Card.Body>
+                                                                    <Card.Text>
+                                                                    <div className="row">
+                                                                        <div className='col-8'>
+                                                                            {item.name}
+                                                                        </div>
+                                                                        <div className="col-2">
+                                                                            <button name={item.id} onClick={CloseOption} className="btn btn-light">
+                                                                                <FontAwesomeIcon icon="arrow-down"/>
+                                                                            </button>
+                                                                        </div>
+                                                                    <div className="col-2">
+                                                                            <button name={item.id} onClick={OpenOption} style={{display : 'block'}} className="btn btn-light">
+                                                                                <FontAwesomeIcon icon="arrow-up"/>
+                                                                            </button>
+                                                                        </div> 
+                                                                    </div>
+                                                                    </Card.Text>
+                                                                    </Card.Body>
+                                                                </Card>
+                                                            </div>
+                                                        );
+                                                    }}
+                                                </Draggable>
+                                            );
+                                            })}
+                                        {provided.placeholder}
+                                    </div>
+                                );
+                            }}
+                        </Droppable>                  
+                    ))}
+                </DragDropContext>
             </div>
         </section>
     )
